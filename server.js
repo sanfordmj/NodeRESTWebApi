431efd11d2fc5462e34b21dc8df7f1c63a81c6de
Michael Sanford <sanford.mj@gmail.com>
2023-10-18 15:44:03 -0500

    Continued development

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 
+//load the modules needed for devlopment
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser'); 
 
+//Configure default port if one is not defined
+const port = process.env.port || process.env.PORT || 5000 
+
+const app = express();
+
+//configure app
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(cors({ origin: /http:\/\/localhost/ }));
+app.options('*', cors());
+
+app.listen(port, () => {
+    console.log('Server up');
+})
