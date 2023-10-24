41abdd98935eba8eddeea529eb5873b47dba9655
Michael Sanford <sanford.mj@gmail.com>
2023-10-24 10:01:54 -0500

    Added Get service, and remove comments

--- server.js
+++ server.js
@@ -3,11 +3,23 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser'); 
+const package = require('./package.json');
+const apiRoot = "/api";
+//config router
+const router = express.Router();
+
+
+router.get("/", (req, res) => {
+    res.send(`${package.description} - v${package.version}`);
+});
 
 //Configure default port if one is not defined
 const port = process.env.port || process.env.PORT || 5000 
 
 const app = express();
+
+//Register router
+app.use(apiRoot, router);
 
 //configure app
 app.use(bodyParser.urlencoded({ extended: true}));
